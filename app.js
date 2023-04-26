f01f306af7ae29c7796d7787c19178c8693d6f31
ripperd-2882 <ripperd2882@gmail.com>
2023-04-27 01:32:10 +0530

    Initial commit

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get("/register", function (req, res) {
     res.render("register");
 })
 
+
 app.post("/register", function (req, res) {
     const user = new User({
         email: req.body.username,
