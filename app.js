2140800c3c5a6197bfbbfd8cfa175bfe09a4be3e
ripperd-2882 <ripperd2882@gmail.com>
2023-04-27 14:17:50 +0530

    Level 3 Hashing

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+var md5 = require('md5');
 require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -19,7 +20,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // console.log(process.env.SECRET); 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,7 +40,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
     const user = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     user.save().then(function () {
@@ -52,7 +53,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }).then(function (foundUser) {
         if (foundUser) {
